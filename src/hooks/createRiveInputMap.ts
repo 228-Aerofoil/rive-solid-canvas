6e86996655150356dc819d0dee5e6af7388db8bd
Michael Kassabov <35264484+Mkassabov@users.noreply.github.com>
2023-02-16 12:27:23 -0500

    add rive component and hooks (#4)

---
+++ src/hooks/createRiveInputMap.ts
@@ -0,0 +1,15 @@
+import type { StateMachineInput, Rive } from "@rive-app/canvas";
+import type { Accessor } from "solid-js";
+
+export function createRiveInputMap(
+	rive: Accessor<Rive | undefined>,
+	stateMachineName: string
+) {
+	return () =>
+		rive()
+			?.stateMachineInputs(stateMachineName)
+			?.reduce((acc, input) => {
+				acc.set(input.name, input);
+				return acc;
+			}, new Map<string, StateMachineInput>());
+}
